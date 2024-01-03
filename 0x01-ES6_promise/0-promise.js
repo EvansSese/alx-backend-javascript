cafe804d1e674ba25c4fd5a7d49128105fb0602c
Evans Sese <sese_evans@yahoo.com>
2024-01-04 00:01:00 +0300

    [Feature] Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const successfulOperation = true;
-    if (successfulOperation) {
-      // If the operation is successful, resolve the promise
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      // If the operation fails, reject the promise
-      reject(new Error('Error'));
-    }
-  });
+  return new Promise(() => {});
 }
