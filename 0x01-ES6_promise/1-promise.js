de98326c90578a6ed8fcd823229737543937b192
Evans Sese <sese_evans@yahoo.com>
2024-01-04 00:05:05 +0300

    [Feature] Task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      // If success is true, resolve the promise with a success object
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      // If success is false, reject the promise with an error object
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
