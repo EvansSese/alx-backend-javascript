219c12299414166a0ed6c85c25aa80eb7c064d0a
Evans Sese <sese_evans@yahoo.com>
2024-01-04 00:30:50 +0300

    [Feature] Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    .then((result) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
+    .catch((error) => {
       console.error('Signup system offline');
     });
 }
