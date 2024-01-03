3e278d8f0c4bec6c1165b74085862c848a92801f
Evans Sese <sese_evans@yahoo.com>
2024-01-04 01:12:00 +0300

    [Feature] Task 10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -7,7 +7,7 @@ export default async function asyncUploadUser() {
     const photo = await uploadPhoto();
     const user = await createUser();
     res = { photo, user };
-  } catch (err) {
+  } catch (error) {
     res = { photo: null, user: null };
   }
   return res;
