23ff36449027998b7626ae508f5c3a6be8d503d6
Evans Sese <sese_evans@yahoo.com>
2024-01-03 21:08:43 +0300

    [Feature] Task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,10 @@
 export default function createIteratorObject(report) {
   const iterator = {
-    [Symbol.iterator]: function* () {
-      for (const department in report.allEmployees) {
-        if (report.allEmployees.hasOwnProperty(department)) {
-          const employees = report.allEmployees[department];
-          for (const employee of employees) {
-            yield employee;
-          }
+    *[Symbol.iterator]() {
+      for (const department of Object.keys(report.allEmployees)) {
+        const employees = report.allEmployees[department];
+        for (const employee of employees) {
+          yield employee;
         }
       }
     },
