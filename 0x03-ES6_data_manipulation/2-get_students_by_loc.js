04846ca4b90734d1f2bd12fe44af717437f3689c
Evans Sese <support@ultimate-tracking.co.ke>
2024-01-08 16:15:36 +0300

    [Feature] Task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(students, location) {
   if (!Array.isArray(students)){
     return []
   }
-  return students.filter(location);
+  return students.filter(student => student.location === location);
 }
