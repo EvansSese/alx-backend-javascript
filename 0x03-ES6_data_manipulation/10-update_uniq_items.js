7ce844913f171f8dfcd4a38c4629fe4bee969947
Evans Sese <sese_evans@yahoo.com>
2024-01-09 22:56:04 +0300

    [Feature] Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,14 @@
 export default function updateUniqueItems(groceryMap) {
   // Check if the argument is a map
   if (!(groceryMap instanceof Map)) {
-      throw new Error('Cannot process');
+    throw new Error('Cannot process');
   }
 
   // Iterate over the map entries and update quantities
   for (const [item, quantity] of groceryMap) {
-      if (quantity === 1) {
-          groceryMap.set(item, 100);
-      }
+    if (quantity === 1) {
+      groceryMap.set(item, 100);
+    }
   }
 
   return groceryMap;
