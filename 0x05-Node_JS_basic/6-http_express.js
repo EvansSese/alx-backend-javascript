c07947af7b8f1ffc5a2ce33833f2f38a5e6fa2b8
Evans Sese <sese_evans@yahoo.com>
2024-02-27 15:24:47 +0000

    [Fix] Lint issue

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,15 +1,16 @@
 const express = require('express');
+
 const app = express();
 
 // Define a route handler for the root path '/'
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 // Set the server to listen on port 1245
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 // Export the Express app instance
