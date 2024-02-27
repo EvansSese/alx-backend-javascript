9d785a1e61527b4c0181f37bf8e3f70f9d4732ac
Evans Sese <sese_evans@yahoo.com>
2024-02-27 18:26:59 +0300

    [Feature] Task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+// Define route handlers
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+    countStudents('database.csv')
+        .then(data => {
+            const totalCount = data.totalCount;
+            const fields = data.fields;
+
+            let response = `This is the list of our students\nNumber of students: ${totalCount}\n`;
+            for (const field in fields) {
+                response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+            }
+            res.send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error.message);
+        });
+});
+
+// Start the server
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
