eede7f6cd2fd283a32f95007b7fc22701f03a628
Evans Sese <sese_evans@yahoo.com>
2024-02-27 18:20:21 +0300

    [Fix] Task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -25,10 +25,9 @@ function countStudents(path) {
                 for (const field in fields) {
                     if (Object.prototype.hasOwnProperty.call(fields, field)) {
                         console.log(
-                            `Number of students in ${field}: ${fields[
-                                field].length}. List: ${fields[field].join(', ')}`,
-                        )
-                    };
+                            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+                        );
+                    }
                 }
 
                 resolve({ totalCount, fields });
