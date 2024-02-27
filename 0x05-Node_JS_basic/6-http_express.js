7471fcb851ecc91e91aa86e9d1888327cfb37667
Evans Sese <sese_evans@yahoo.com>
2024-02-27 17:08:46 +0300

    [Feature] Task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Define a route handler for the root path '/'
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+// Set the server to listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Export the Express app instance
+module.exports = app;
