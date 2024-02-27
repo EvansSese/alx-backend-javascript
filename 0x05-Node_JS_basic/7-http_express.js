3ef2bff00846f5f5e4216f9119efe3f7323fa748
Evans Sese <sese_evans@yahoo.com>
2024-02-27 15:30:39 +0000

    [Fix] Lint issue

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,32 +5,32 @@ const app = express();
 
 // Define route handlers
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    countStudents('database.csv')
-        .then(data => {
-            const totalCount = data.totalCount;
-            const fields = data.fields;
+  countStudents('database.csv')
+    .then((data) => {
+      const { totalCount } = data;
+      const { fields } = data;
 
-            let response = `This is the list of our students\nNumber of students: ${totalCount}\n`;
-            for (const field in fields) {
-                if (Object.prototype.hasOwnProperty.call(fields, field)) {
-                    response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-                }
-            }
-            res.send(response);
-        })
-        .catch(error => {
-            res.status(500).send(error.message);
-        });
+      let response = `This is the list of our students\nNumber of students: ${totalCount}\n`;
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        }
+      }
+      res.send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
 });
 
 // Start the server
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
