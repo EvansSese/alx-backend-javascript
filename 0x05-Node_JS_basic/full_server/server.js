c13d930030b6cb3a10e99bb4b47fadde569f6926
Evans Sese <sese_evans@yahoo.com>
2024-02-27 15:43:04 +0000

    [Fix] Lint issue

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -6,14 +6,14 @@ const PORT = process.env.PORT || 1245;
 const DATABASE_PATH = process.argv[2];
 
 app.use((req, res, next) => {
-    req.databasePath = DATABASE_PATH;
-    next();
+  req.databasePath = DATABASE_PATH;
+  next();
 });
 
 app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
